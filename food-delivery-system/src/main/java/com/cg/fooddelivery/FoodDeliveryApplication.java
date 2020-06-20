5fcf869debae664869ed2e043a489094a8128718
FoodDeliveryApplication <66238905+FoodDeliveryApplication@users.noreply.github.com>
2020-06-20 13:32:18 +0530

    added comment messages

--- food-delivery-system/src/main/java/com/cg/fooddelivery/FoodDeliveryApplication.java
+++ food-delivery-system/src/main/java/com/cg/fooddelivery/FoodDeliveryApplication.java
@@ -8,6 +8,8 @@ public class FoodDeliveryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FoodDeliveryApplication.class, args);
+		System.out.println("welcome to Food application system ");
+		System.out.println("Hungry ??? \n \n Explore the best food and drinks from Multi Cuisine Restaurants");
 
 	}
 
